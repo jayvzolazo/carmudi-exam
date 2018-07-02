c63d1421394134b09dac17bf0ac77e024effab62
Jayvz Olazo <jayvzolazo@gmail.com>
2018-07-02 12:30:26 +0800

    FRONTEND - code cleanup

--- carmudi-app/src/app/cars-list/cars-list.component.ts
+++ carmudi-app/src/app/cars-list/cars-list.component.ts
@@ -8,16 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CarsListComponent implements OnInit {
   @Input() carsList: Array<object> = [];
 
-  // objectList: Array<object> = [];
-
   constructor() { }
 
-  // private buildObjects(): void {
-  //   let objectList = this.carsList;
-  // }
-
-  ngOnInit() {
-    // this.buildObjects();
-  }
+  ngOnInit() { }
 
 }
--- carmudi-app/src/app/services/car.service.ts
+++ carmudi-app/src/app/services/car.service.ts
@@ -21,7 +21,6 @@ export class CarService {
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
-      // this.log(`${operation} failed: ${error.message}`);
       return of(result as T);
     };
   }
